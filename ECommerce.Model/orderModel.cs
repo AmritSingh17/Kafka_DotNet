421b3df65b419c7c9fac7802ccc549d6421b0c66
Amrit Singh <amritsinghy53@gmail.com>
2026-02-22 17:22:43 +0530

    Adding models

---
+++ ECommerce.Model/orderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Model
+{
+    internal class orderModel
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+
+    }
+}
---
+++ ECommerce.Model/productModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Model
+{
+    internal class productModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
