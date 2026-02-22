0a7ad4390acaab473c32c5bfa79a9f3225a8a623
Amrit Singh <amritsinghy53@gmail.com>
2026-02-22 17:02:19 +0530

    Added gitignore

---
+++ ECommerce.AppHost/AppHost.cs
@@ -0,0 +1,8 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var productApi = builder.AddProject<Projects.ECommerce_ProductService>("apiservice-product");
+var orderApi = builder.AddProject<Projects.ECommerce_OrderService>("apiservice-order");
+
+builder.AddProject<Projects.ECommerce_Web>("webFrontend")
+    .WithReference(productApi) .WithReference(orderApi);
+builder.Build().Run();
